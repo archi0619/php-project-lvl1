9b3f70283b62cbb56735620d89a09aaea531e258
artem alekseev <temchic1@gmail.com>
2022-06-09 00:28:37 +0300

    Added Codesniffer

--- src/Cli.php
+++ src/Cli.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Src\Cli;
 
 use function cli\line;
@@ -6,7 +7,7 @@ use function cli\prompt;
 
 function greeting()
 {
-	line('Welcome to the Brain Game!');
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
