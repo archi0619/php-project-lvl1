daa49a613c9a35f77d5574003f31df58148129c3
artem alekseev <temchic1@gmail.com>
2022-06-27 15:49:37 +0300

    Refactoring brain-prime game

---
+++ src/Games/Prime.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Src\Games\Prime;
+
+use function cli\line;
+use function cli\prompt;
+use function Src\Engine\greeting;
+
+function primeCheck($num)
+{
+    for ($i = 2; $i < $num / 2; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function brainPrime()
+{
+    $name = greeting();
+    line('Answer "yes" if given number is prime. Otherwise answer "no".');
+    
+    for ($i = 0; $i < 3; $i++) {
+        $num = rand($min = 2, $max = 50);
+        line("Question: $num");
+        $answer = prompt("Your answer: ");
+
+        if (primeCheck($num)) {
+            $correctAnswer = "yes";
+        } else {
+            $correctAnswer = "no";
+        }
+
+        if (primeCheck($num) && $answer === $correctAnswer || !primeCheck($num) && $answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            return line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.\nLet's try again, $name!");
+        }
+    };
+    
+    line("Congratulations, $name!");
+}
