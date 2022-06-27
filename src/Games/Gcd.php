362d220b9a1da7d3bf7ab29c028989a035aeee2d
artem alekseev <temchic1@gmail.com>
2022-06-27 15:04:32 +0300

    Refactoring brain-gcd game

---
+++ src/Games/Gcd.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Src\Games\Gcd;
+
+use function cli\line;
+use function cli\prompt;
+use function Src\Engine\greeting;
+
+function brainGCD()
+{
+    $name = greeting();
+    line('Find the greatest common divisor of given numbers.');
+    for ($i = 0; $i < 3; $i++) {
+        $num1 = rand($min = 1, $max = 50);
+        $num2 = rand($min = 1, $max = 50);
+        $question = "{$num1} {$num2}";
+        line("Question: {$question}");
+        $answer = prompt("Your answer");
+        $correctAnswer = gcd($num1, $num2);
+        if ($answer == $correctAnswer) {
+            line('Correct!');
+        } else {
+            return line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.\nLet's try again, $name!");
+        }
+    }
+    line("Congratulations, $name!");
+}
+
+function gcd($a, $b)
+{
+    return ($a % $b) ? gcd($b, $a % $b) : $b;
+}
