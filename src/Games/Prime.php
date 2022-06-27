06b230a0c31e4c22fb2131d116ac95c6c3101d52
artem alekseev <temchic1@gmail.com>
2022-06-27 17:06:08 +0300

    Refactoring Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -20,13 +20,11 @@ function brainPrime()
 {
     $name = greeting();
     line('Answer "yes" if given number is prime. Otherwise answer "no".');
-    
     for ($i = 0; $i < 3; $i++) {
         $num = rand($min = 2, $max = 50);
         line("Question: $num");
         $answer = prompt("Your answer ");
         $correctAnswer = primeCheck($num) ? 'yes' : 'no';
-
         if ($answer == $correctAnswer) {
             line('Correct!');
         } else {
