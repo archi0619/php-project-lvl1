14418dc144d9c07bd3bf4d8d05a419cd81cf720a
artem alekseev <temchic1@gmail.com>
2022-06-27 14:50:38 +0300

    Refactoring brain-even game

---
+++ src/Games/Even.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Src\Games\Even;
+
+use function cli\line;
+use function cli\prompt;
+use function Src\Engine\greeting;
+
+function isEven()
+{
+    $name = greeting();
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    for ($i = 0; $i < 3; $i++) {
+        $num = rand($min = 1, $max = 30);
+        $correctAnswer = $num % 2 === 0 ? 'yes' : 'no';
+        line("Question: $num");
+        $answer = prompt("Your answer: ");
+        if ($answer === $correctAnswer) {
+            line('Correct!');
+        } else {
+            return line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.\nLet's try again, $name!");
+        }
+    };
+    line("Congratulations, $name!");
+};
