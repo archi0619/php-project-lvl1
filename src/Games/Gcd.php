a53aee580b8c54e74923cb58265a26ee1e345d3d
artem alekseev <temchic1@gmail.com>
2022-06-28 16:44:31 +0300

    Update Calc and Src

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -7,7 +7,7 @@ use function Src\Engine\playGame;
 const GAME_NAME = 'Gcd';
 const GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
-function play(): void
+function play(): mixed
 {
     $generateTask = function (): array {
         $number1 = rand(1, 99);
